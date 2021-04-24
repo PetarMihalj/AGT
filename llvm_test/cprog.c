739d1ad9dd0cf82b02cf608e31f507ca2251c401
Petar Mihalj <git@pmihalj.com>
2021-04-24 10:48:34 +0200

    cleanup bytecode

--- llvm_test/cprog.c
+++ llvm_test/cprog.c
@@ -1,14 +1,30 @@
 #include <stdlib.h>
 
-void copy(int* a, int* b){
+int copy(int* a, int* b){
     *a = *b;
+    return 1;
 }
 
+struct rect{
+    int a;
+};
+
+struct rect* alloc_i32(int size){
+    struct rect* psInfo;
+    int iSizeofStructure = (char*)(psInfo + 1) - (char*)(psInfo);
+    return (struct rect*)malloc(iSizeofStructure * size);
+}
+
+
 int main(){
-    int* a = (int*) malloc(sizeof(char) * 3);
+    int ps = sizeof(char*);
+    int* a = (int*) malloc(sizeof(int) * 3);
+    free(a);
     int b=5;
     int c=10;
-    int d=b+c;
+    int d =copy(&b, &c);
+
+    struct rect r;
     
 
 }
