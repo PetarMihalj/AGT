18ade2807d2886a36dab0cfb6c13caa76098cc8c
Petar Mihalj <git@pmihalj.com>
2021-05-08 13:02:13 +0200

    fixed bib style

--- paper/continous.sh
+++ paper/continous.sh
@@ -2,5 +2,5 @@ export THESIS_SRCDIR=$(readlink -f './source')
 export THESIS_BUILDDIR=$(readlink -f './build')
 export THESIS_RESDIR=$(readlink -f './res')
 
-(cd $THESIS_SRCDIR && latexmk -pdf -pvc -interaction=nonstopmode \
-    -output-directory=$THESIS_BUILDDIR diplomski.tex)
+(cd $THESIS_SRCDIR && latexmk -bibtex -pdf -pvc \
+    -interaction=nonstopmode -output-directory=$THESIS_BUILDDIR diplomski.tex)
--- paper/once.sh
+++ paper/once.sh
@@ -2,4 +2,5 @@ export THESIS_SRCDIR=$(readlink -f './source')
 export THESIS_BUILDDIR=$(readlink -f './build')
 export THESIS_RESDIR=$(readlink -f './res')
 
-(cd $THESIS_SRCDIR && pdflatex -interaction=nonstopmode -output-directory=$THESIS_BUILDDIR diplomski.tex)
+(cd $THESIS_SRCDIR && latexmk -bibtex -pdf\
+    -interaction=nonstopmode -output-directory=$THESIS_BUILDDIR diplomski.tex)
